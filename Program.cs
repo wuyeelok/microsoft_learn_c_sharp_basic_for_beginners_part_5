7b668e3eb4d789c9b32a3743f48ac731a4611197
wuyeelok <wuyeelok@gmail.com>
2025-02-17 12:09:08 +0800

    Testing String Trim function

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! Part 5");
 
-string firstFriend = "\"James\"";
-string secondFriend = "\"Mary\"";
-Console.WriteLine($"My friends are {firstFriend} and {secondFriend}. {{  }}");
-Console.WriteLine("My friends are " + firstFriend + " and " + secondFriend + ". {  }");
+string firstFriend = "      \"James\" ";
+string secondFriend = "  \"Mary\"       ";
+Console.WriteLine($"My friends are {firstFriend.Trim()} and {secondFriend.Trim()}. {{  }}");
+Console.WriteLine("My friends are " + firstFriend.TrimStart() + " and " + secondFriend.TrimEnd() + ". {  }");
+
+var name = "    Mark    ";
+name = name.Trim();
+Console.WriteLine($"Hello, {name}!");
