e76a39b492d92849eec2f8c6bf4567880d5f8297
wuyeelok <wuyeelok@gmail.com>
2025-02-17 11:56:55 +0800

    String interpolation using $

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! Part 5");
+
+string firstFriend = "\"James\"";
+string secondFriend = "\"Mary\"";
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}. {{  }}");
+Console.WriteLine("My friends are " + firstFriend + " and " + secondFriend + ". {  }");
